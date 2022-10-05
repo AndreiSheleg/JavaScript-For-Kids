39f057314572533af8d5e21fb398f84da928e66c
Andrei <106683554+AndreiSheleg@users.noreply.github.com>
2022-10-05 09:44:22 +0300

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,8 +1,8 @@
 //РИСУЕМ СТОЛЬКО КОТИКОВ, СКОЛЬКО ЗАХОТИМ!
 var drawCats = function (howManyTimes) {
-for  (var i = 0; i < howManyTimes < i++) {
+for  (var i = 0; i < howManyTimes; i++) {
   console.log (i + " =^..^=");
 }
   };
 
-drawCats (10); //вместо 10 тут может быть другое число
+drawCats(10); //вместо 10 тут может быть другое число
